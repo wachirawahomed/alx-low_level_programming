60fb606947c0acf27ee7999b9aeb67fa334b26ac
wachirawahomed <wachirawahomed@gmail.com>
2023-07-14 02:37:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" Dora Korpar, 2015-10-19",
-			strlen("and that piece of art is useful
-				\" Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
 	return (1);
 }
