c81624651edaeffbbe5e523373eafd1ff46f115f
wachirawahomed <wachirawahomed@gmail.com>
2023-07-14 15:10:31 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("%d \n", n);
+	printf("\r %d", n);
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf(" is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf(" is zero\n");
 	}
 	else
 	{
-		 printf("is negative\n");
+		 printf(" is negative\n");
 	}
 	return (0);
 }
