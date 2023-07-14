32fa181c9c558e3929f8c1e931fe958a30ac891e
wachirawahomed <wachirawahomed@gmail.com>
2023-07-13 19:09:52 -0700

    100-intel

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,4 +1,5 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.globl	main
 	.type	main, @function
@@ -6,16 +7,16 @@ main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	movl	$0, %edi
-	movl	$0, %eax
+	mov	edi, 0
+	mov	eax, 0
 	call	retun@PLT
-	movl	$0, %eax
-	popq	%rbp
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
