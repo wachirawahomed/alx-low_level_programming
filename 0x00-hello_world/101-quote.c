134367883f91bb326826ead114067380abfefac7
wachirawahomed <wachirawahomed@gmail.com>
2023-07-13 20:09:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	fwrite(1, "and that piece of art is useful\" 
-			- Dora Korpar, 2015-10-19", 60);
+	write(1, "and that piece of art is useful\" Dora Korpar, 2015-10-19",
+			strlen("and that piece of art is useful
+				\" Dora Korpar, 2015-10-19"));
 	return (1);
 }
