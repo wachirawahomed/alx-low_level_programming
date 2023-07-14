3049a28a8c980c3e53e64a9cc019c9e20ab56a1b
wachirawahomed <wachirawahomed@gmail.com>
2023-07-13 18:48:46 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *sizeof operator
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %2d byte\(s\) \n", sizeof(char));
+	printf("Size of an int: %2d byte\(s\) \n", sizeof(short int));
+	printf("Size of a long int: %2d byte\(s\) \n", sizeof(long int));
+	printf("Size of a long long int:  %2d byte\(s\)\n", sizeof(double));
+	printf("Size of a float: %2d byte\(s\) \n", sizeof(float));
+	return (0);
+}
