010ef178ba146ce004b88389b9eb7a4fc4cff697
wachirawahomed <wachirawahomed@gmail.com>
2023-07-14 18:08:33 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%i", num);
+	}
+	putchar('\n');
+	return (0);
+}
