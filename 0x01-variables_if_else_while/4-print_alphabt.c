3cd1945daa4cb0b84c0d8cedb4eadf5e9353344c
wachirawahomed <wachirawahomed@gmail.com>
2023-07-14 17:51:32 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; ++al)
+	{
+		if (al != 'e' && al != 'q')
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
