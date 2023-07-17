c02c78d3259d2b1d8efa9a6b456262a892a3667e
wachirawahomed <wachirawahomed@gmail.com>
2023-07-17 08:39:28 -0700

    6-size.c revision

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,7 @@ int main(void)
 	printf("Size of an int: %2d byte\(s\)\n", sizeof(int));
 	printf("Size of a long int: %2d byte\(s\)\n", sizeof(long int));
 	printf("Size of a long long int: %2d byte\(s\)\n", sizeof(double));
+	
+	printf("Size of a float: %2d byte\(s\)\n", sizeof(float));
 	return (0);
 }
