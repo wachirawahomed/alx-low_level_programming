f766a81aa7514a9de4ef8c9c7e594ee19f7fbcc3
wachirawahomed <wachirawahomed@gmail.com>
2023-07-17 13:51:08 -0700

    102 print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,25 +6,24 @@
  */
 int main(void)
 {
-	int i, j;
+	int a, b;
 
-    for (i = 0; i <= 99; i++)
-        {
-            for (j = i + 1; j <= 99; j++)
-            {
-                putchar((i / 10) + '0');
-                putchar((i % 10) + '0');
-                putchar(' ');
-                putchar((j / 10) + '0');
-                putchar((j % 10) + '0');
-
-                if (i < 98 || j < 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }	
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a < 98 || b < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
