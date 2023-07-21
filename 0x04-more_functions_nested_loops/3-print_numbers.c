84fe9b5188b55d2f585e0ad50225626553dfc4f9
wachirawahomed <wachirawahomed@gmail.com>
2023-07-20 18:53:14 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ *
+ */
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		_putchar(n);
+
+	_putchar('\n');
+}
