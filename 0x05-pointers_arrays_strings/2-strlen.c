e75b774c0a1da8d77e9bfe7fac2a96bde96c659f
wachirawahomed <wachirawahomed@gmail.com>
2023-07-24 15:52:16 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string.
- *
+ *@s: a string.
  *Return: length of a string.
  */
 
@@ -15,6 +15,5 @@ int _strlen(char *s)
 		l++;
 		s++;
 	}
-
 	return (l);
 }
