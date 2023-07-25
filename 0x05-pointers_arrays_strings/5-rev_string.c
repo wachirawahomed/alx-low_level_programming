96ac648e0492c909589902a71e8915a33f6b6a68
wachirawahomed <wachirawahomed@gmail.com>
2023-07-25 04:33:23 +0300

    all

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,17 +2,18 @@
 
 /**
  * rev_string - function that reverses a string.
- * @s - the string
+ * @s - the string to be reversed
  *
+ *return: 0 always
  */
 void rev_string(char *s)
 {
 	int i, len, temp;
-	
+
 	for (len = 0; s[len] != '\0'; len++)
 	{
 	}
-	for (i = 0; i < len/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[len - i - 1];
