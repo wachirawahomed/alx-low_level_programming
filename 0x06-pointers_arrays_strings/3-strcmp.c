c11ea1dedccc9cb71e0dd14f20e780ab268bb842
wachirawahomed <wachirawahomed@gmail.com>
2023-07-28 05:25:57 +0300

    morning rush 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
-	int index;
-
-	while (s2[index] == s1[index] && s1[index] != '\0')
+	while (*s1 && *s2)
 	{
-		index++;
+		if (*s1 - *s2 != 0)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-
-	res = s1[index] - s2[index];
-
-	return (res);
+	return (0);
 }
