146d3c201cd3607d0f04edd49d2cada24fe6ee73
wachirawahomed <wachirawahomed@gmail.com>
2023-07-28 05:50:32 +0300

    100 rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,14 +10,17 @@ char *rot13(char *s)
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		 i = 0;
-		 while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
-			 i++;
-
-		 if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
-			 s[j] = b[i];
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+		{
+			i++;
+		}
+		if (s[j] == a[i])
+		{
+			s[j] = b[i];
+		}
 	}
 	return (s);
 }
