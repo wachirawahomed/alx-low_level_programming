327efc019b25162c8644738a8fa16279317f33c1
wachirawahomed <wachirawahomed@gmail.com>
2023-08-02 20:01:17 -0700

    8

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * palindrome - compares the first and last character in a string
+ * @al: first index in string
+ * @om: last index in a string.
+ * Return: 1 if string is a palindrome, 0 otherwise.
+ */
+
+int palindrome(int al, int om)
+{
+	if (al >= om)
+		return (1);
+	if (al == om)
+		return ( palindrome (al + 1, om -1));
+	else
+		return (0);
+}
+
+
+/**
+ * _slength - finds lenght of the string
+ * @s: string
+ * Return: lenth of string.
+ */
+
+int _slength(char *s)
+{
+	if (*s != '\0')
+		s++;
+		return (1 + (_slength(s)));
+	else
+		return (0);
+}
+		
+/**
+ * is_palindrome -  returns 1 if a string is a palindrome and 0 if not.
+ * @s: string
+ * Return: 1 if true otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	int l = _slenght(s);
+	return (palindrome (s , (s + (l - 1)));
+}
