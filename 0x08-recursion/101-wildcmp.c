85267eaeced23f5ebc9f303d882e0e8e6d3199eb
wachirawahomed <wachirawahomed@gmail.com>
2023-08-04 04:31:01 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,5 +15,12 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s1 == '\0')
+	{
+		if (*s2 != '*')
+			return (0);
+		else
+			return (wildcmp(s1, s2 + 1));
+	}
 	return (0);
 }
