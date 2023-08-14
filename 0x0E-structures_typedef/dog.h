1e7d6b3a4c1f14bc114feff4e5d184ac27097360
wachirawahomed <wachirawahomed@gmail.com>
2023-08-15 01:52:35 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Defines a new type struct dog
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
