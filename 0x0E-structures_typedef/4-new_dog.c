7ffa2940b8c04f4e563f08bbcd8d516d9ad64fdb
wachirawahomed <wachirawahomed@gmail.com>
2023-08-15 03:47:24 +0300

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * stlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int stlen(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i); i++)
+		;
+	return (i);
+}
+
+/**
+ * new_dog - creates a new dog.
+ * @name: pointer to dog's name
+ * @age: dog's age
+ * @owner: pointer to dog's owner
+ * Return: pointer to new dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dg;
+	int i;
+	int lname = stlen(name);
+	int l_own = stlen(owner);
+
+	dg = (dog_t *)malloc(sizeof(dog_t));
+	if (dg == NULL)
+		return (NULL);
+
+	dg->name = malloc(lname * sizeof(char));
+	if (dg->name == NULL)
+	{
+		free(dg);
+		return (NULL);
+	}
+	for (i = 0; i <= lname; i++)
+		dg->name[i] = name[i];
+	dg->age = age;
+	dg->owner = malloc(l_own * sizeof(char));
+	if (dg->owner == NULL)
+	{
+		free(dg->name);
+		free(dg);
+		return (NULL);
+	}
+	for (i = 0; i <= l_own; i++)
+		dg->owner[i] = owner[i];
+	return (dg);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - Defines a new type struct dog
+ * struct dog - Defines a new type struct dog.
  * @name: the dog's name.
  * @age: the dog's age.
- * @owner: the dog's owner
+ * @owner: the dog's owner.
  *
  */
 
@@ -19,8 +19,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 /**
- * dog_t - a new name for the type struct dog.
+ * dog_t - a new name for the type struct dog..
  */
 typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
