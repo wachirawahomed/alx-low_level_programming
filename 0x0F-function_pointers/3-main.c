709c28a26126a7a649a603283b5fa6488660e047
wachirawahomed <wachirawahomed@gmail.com>
2023-08-17 02:50:25 +0300

    all no.3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+
+/**
+ * main - prints results of operation on two intergers.
+ * @argc: number of arguments.
+ * @argv: pointer to an array of arguments.
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *op;
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - finds the sum of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds the difference of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - finds the producet of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: division of a by b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds remainder of the division of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
