db0b59d5aa93f100a29c6a284a4421fe681f25ba
wachirawahomed <wachirawahomed@gmail.com>
2023-09-05 23:25:59 +0300

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-				len++;
+			len++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
