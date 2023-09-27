2a2a2ebdb4de0cf164479804e3290ce57434deb0
wachirawahomed <wachirawahomed@gmail.com>
2023-09-28 02:12:14 +0300

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,18 +9,6 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-	return (*head);
+	head++;
+	return (0);
 }
