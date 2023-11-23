844c6dd2c53a96fb26f4456f55175bcabe8b5c2d
wachirawahomed <wachirawahomed@gmail.com>
2023-11-23 18:59:40 +0300

    0-binary_to_uint.c betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int n;
-        unsigned int dec_val = 0;
+	int n;
+	unsigned int dec_val = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (n = 0; b[n]; n++)
-        {
-                if (b[n] < '0' || b[n] > '1')
-                        return (0);
-                dec_val = 2 * dec_val + (b[n] - '0');
-        }
-        return (dec_val);
+	for (n = 0; b[n]; n++)
+	{
+		if (b[n] < '0' || b[n] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[n] - '0');
+	}
+	return (dec_val);
 }
