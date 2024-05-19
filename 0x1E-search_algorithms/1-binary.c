da1c232c19f84b65a24fc7b8fd7616f6da0b3449
wachirawahomed <wachirawahomed@gmail.com>
2024-05-19 21:34:50 +0300

    task 0-6

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers using
+ *                 binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index of the value in the array, or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1, mid, i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+
+		mid = (left + right) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,20 @@
+# Search Algorithms
+
+This repository contains implementations of various search algorithms in C programming language. Each algorithm is implemented in a separate C file and is thoroughly documented.
+
+## Description
+
+This project aims to provide efficient implementations of various search algorithms. Each algorithm is designed to find a target value within a collection of data.
+
+## Implemented Algorithms
+
+The following search algorithms are implemented in this repository:
+
+- Linear Search
+- Binary Search
+- Jump Search
+- Interpolation Search
+- Exponential Search
+- Advanced Binary Search
+- Jump Search in a Singly Linked List
+- Linear Search in a Skip List
